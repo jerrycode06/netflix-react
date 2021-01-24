5882bcfc6857688c9bd31da6b6c86231c598b444
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-01-25 01:51:43 +0530

    Sign Up Page Added

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -58,7 +58,7 @@ export default function SignIn() {
             </Form.Submit>
           </Form.Base>
           <Form.Text>
-            New to Netflix? <Form.Link to="/signup">Sign Up Now.</Form.Link>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
           </Form.Text>
           <Form.TextSmall>
             This page is protected by Google reCAPTCHA to ensure you're not a
