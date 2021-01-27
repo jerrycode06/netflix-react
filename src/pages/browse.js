f6d21b9d5cc2e6911ac4e21a293ca7b498a76225
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-01-28 00:15:09 +0530

    Profiles Page added

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { BrowseContainer } from "../containers/browse";
+import { useContent } from "../hooks";
+import selectionFilter from "./../utils/selection-filter";
 
 export default function Browse() {
   // We need series and films
+  const { series } = useContent("series");
+  const { films } = useContent("films");
   // we need slides
+  const slides = selectionFilter({ series, films });
   //pass it to the browse container
 
-  return <h1>Hello from the Browse</h1>;
+  return <BrowseContainer slides={slides} />;
 }
